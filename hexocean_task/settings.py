4d5aeba812e2c7adbe894cec9fe0c380c2dc094b
Michał Roszkowski <michalr4550@gmail.com>
2022-06-01 22:05:57 +0200

    heroku update

--- hexocean_task/settings.py
+++ hexocean_task/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = str(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'hexocean-task-mr.herokuapp.com']
 
